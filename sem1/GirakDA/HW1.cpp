543fe1fb0a8e29e359e1c236fc7d5c9806a3301b
Dmitriy Girak <rodnayasem@gmail.com>
2025-10-11 21:05:27 +0300

    Done HW1

---
+++ sem1/GirakDA/HW1.cpp
@@ -0,0 +1,28 @@
+//ploshad kvadrata//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  float storona;
+  cout << "Vvedite storony kvadrata = ";
+  cin >> storona;
+  cout << "Ploshad vashego kvadrata = " << storona * storona << endl;
+}
+//ploshad trapecii//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  float h, a, b;
+  cout << "Vvedite visoty = ";
+  cin >> h;
+  cout << "Vvedite pervyu storony = ";
+  cin >> a;
+  cout << "Vvedite vtoryu storony = ";
+  cin >> b;
+  cout << "Ploshad trapecii = " << (0.5 * (a + b)) * h << endl;
+}

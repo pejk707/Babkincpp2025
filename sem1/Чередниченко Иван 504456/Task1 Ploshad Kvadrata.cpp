2048ddd271b3b6a9961026584114fc72d4ab5964
cherednichenkobrand <provizorvanya@mail.ru>
2025-10-11 12:18:10 +0300

    Мини-домашка 1
    Иван Чередниченко - крутой чел сделал первую мини домашку)
    
    Решкны задачи на определение площади квадрата и трапеции

---
+++ sem1/Чередниченко Иван 504456/Task1 Ploshad Kvadrata.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int S(int a, int b)
+{
+    cout << "S = " << a * b;
+    return 0;
+}
+
+int main()
+{
+    int a, b;
+    cout << "a = ";
+    cin >> a;
+    
+    cout << "b = ";
+    cin >> b;
+    S(a, b);
+}
+
---
+++ sem1/Чередниченко Иван 504456/Task2 Ploshad Trapicii.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int S(int a, int b, int h) 
+{
+    cout << ((a + b) * h) / 2;
+    return 0;
+}
+
+int main()
+{
+    setlocale(LC_ALL,"rus");
+    int a, b, h;
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+    cout << "h = ";
+    cin >> h;
+
+    cout << "S = ";
+
+    S(a, b, h);
+}
+

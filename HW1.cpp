ed88d95e1b0d4d8ebce291d85c4ebf4aafd102b8
IFanTasmAF <akhmetov129@gmail.com>
2025-10-11 15:31:53 +0300

    HM1 Akhmetov
    Первая домашка на лучшем языке

---
+++ HW1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+double squareA(double x)
+{
+    return(x * x);
+}
+double trapeze(double x, double y, double h)
+{
+    return((x + y) / 2 * h);
+}
+int main()
+{
+    system("chcp 1251");
+    system("cls");
+
+    double x = 0;
+    double y = 0;
+    double h = 0;
+    int num = 0;
+
+    std::cout << "Найти площадь:  1.Квадрата  2.Трапеции";
+    std::cin >> num;
+    switch (num)
+    {
+    case (1):
+            std::cout << "Введите длину стороны квадрата:\n";
+            std::cin >> x;
+            std::cout << "Площадь квадрата равна = " << squareA(x);
+            break;
+        
+    case (2):
+            std::cout << "Введите длину первого основания трапеции:\n";
+            std::cin >> x;
+            std::cout << "Введите длину второго основания квадрата:\n";
+            std::cin >> y;
+            std::cout << "Введите длину высоты трапеции:\n";
+            std::cin >> h;
+            std::cout << "Площадь трапеции равна = " << trapeze(x, y, h) << std::endl;
+            break;
+    }
+    return 0;
+}

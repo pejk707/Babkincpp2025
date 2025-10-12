e565336c497087305761552649f259bf1d907746
Polsha228 <118690406+Polsha228@users.noreply.github.com>
2025-10-12 19:07:11 +0300

    дз 1 и 2

---
+++ sem1/GerasimovaPV/homework2_2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+bool cracuz(int c)
+{
+	bool result;
+	while (c > 1)
+	{
+		if (c % 2 == 0)
+		{
+			c /= 2;
+		}
+		else if (c < 1)
+		{
+			break;
+		}
+		else
+		{
+			c = c * 3 + 1;
+		}
+	}
+	if (c == 1)
+	{
+		result = true;
+	}
+	return result;
+}
+
+int main()
+{
+	int c;
+	std::cin >> c;
+	std::cout << cracuz(c);
+}

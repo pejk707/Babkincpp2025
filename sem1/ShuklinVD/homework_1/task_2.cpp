a3e5d1b36bfdbd7c445d461fcc0ec2699aab1df9
Vovok777 <vladimir.shuklin77@mail.ru>
2025-10-14 17:07:22 +0300

    I have revised this task according to Google standards.

---
+++ sem1/ShuklinVD/homework_1/task_2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <locale>
+float areaTrapezoid(float bigSide, float smallSide, float height) {
+  if (bigSide > 0 and smallSide > 0 and height > 0) {
+    return 0.5 * (bigSide + smallSide) * height;
+  } else {
+    return 0;
+  }
+}
+int main() {
+  setlocale(LC_ALL, "");
+  float trapezoidBigSide;
+  float trapezoidSmallSide;
+  float trapezoidHeight;
+  std::cout << "Введи большую сторону трапеции ";
+  std::cin >> trapezoidBigSide;
+  std::cout << "Введи малую сторону трапеции ";
+  std::cin >> trapezoidSmallSide;
+  std::cout << "Введи высоту сторону трапеции ";
+  std::cin >> trapezoidHeight;
+  if (areaTrapezoid(trapezoidBigSide, trapezoidSmallSide, trapezoidHeight)) {
+    std::cout << areaTrapezoid(trapezoidBigSide, trapezoidSmallSide,
+                               trapezoidHeight)
+              << " - площадь трапеции";
+  } else {
+    std::cout
+        << "Ошибка: в геометрии нет чисел меньше 0 или 0 - размер стороны";
+  }
+  return 0;
+}

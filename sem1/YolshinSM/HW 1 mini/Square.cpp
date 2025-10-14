9e8de66bdd64b95099c91b9dcde35ebede35ff15
Saldaw <yolshin07@bk.ru>
2025-10-15 00:21:14 +0300

    Fix formating

--- sem1/YolshinSM/HW 1 mini/Square.cpp
+++ sem1/YolshinSM/HW 1 mini/Square.cpp
@@ -2,11 +2,10 @@
 
 int square(int a) { return a * a; }
 
-int main()
-{
-    int a = 0;
-    std::cout << "Enter side: ";
-    std::cin >> a;
-    std::cout << "Area of square: ";
-    std::cout << square(a);
+int main() {
+  int a = 0;
+  std::cout << "Enter side: ";
+  std::cin >> a;
+  std::cout << "Area of square: ";
+  std::cout << square(a);
 }
--- sem1/YolshinSM/HW 2/Syracuse tester.cpp
+++ sem1/YolshinSM/HW 2/Syracuse tester.cpp
@@ -1,23 +1,18 @@
-﻿#include <iostream>
+#include <iostream>
 
-int main()
-{
-    int a = 0;
-    int i = 0;
-    std::cout << "Enter number: ";
-    std::cin >> a;
-    while (a != 1) 
-    {
-        i++;
-        if (a % 2 == 0) 
-        {
-            a /= 2;
-        }
-        else
-        {
-            a = (a * 3 + 1) / 2;
-        }
-        std::cout << a << " ";
+int main() {
+  int a = 0;
+  int i = 0;
+  std::cout << "Enter number: ";
+  std::cin >> a;
+  while (a != 1) {
+    i++;
+    if (a % 2 == 0) {
+      a /= 2;
+    } else {
+      a = (a * 3 + 1) / 2;
     }
-    std::cout << "\nIt works!\n" << i << " transformations were required!";
+    std::cout << a << " ";
+  }
+  std::cout << "\nIt works!\n" << i << " transformations were required!";
 }

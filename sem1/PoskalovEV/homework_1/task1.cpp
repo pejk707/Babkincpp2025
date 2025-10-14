4772afdb61b3dbdc75e852d2b6cb0beb60c61343
Xiada <237393679+xiakarpa@users.noreply.github.com>
2025-10-14 17:51:21 +0300

    style: clang-formatted code with Google-style

--- sem1/PoskalovEV/homework_1/task1.cpp
+++ sem1/PoskalovEV/homework_1/task1.cpp
@@ -1,13 +1,10 @@
-float pow2(float val) {
-	return val * val;
-}
-
+float pow2(float val) { return val * val; }
 
 /* Реализовать функцию, которая возвращает подсчет площади квадрата. */
 
 #include <iostream>
 
 int main() {
-	std::cout << pow2(6) << std::endl;
-	return 0;
+  std::cout << pow2(6) << std::endl;
+  return 0;
 }

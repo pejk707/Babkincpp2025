1b8de8c41396f0d4896d4e14b9999b3fe54b1811
Xiada <237393679+xiakarpa@users.noreply.github.com>
2025-10-14 17:48:30 +0300

    style: clang-formatted code with Google-style

--- sem1/PoskalovEV/homework_2/task1.cpp
+++ sem1/PoskalovEV/homework_2/task1.cpp
@@ -2,40 +2,35 @@
 
 const static std::string alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
 
-std::string decimal_to_base(int number, unsigned int base) {
-	if (base >= alphabet.length()) return "";
-	
-	std::string result = "";
-	
-	if (number < 0) {
-		result += '-';
-	}
-	
-	while (number > 0) {
-		result += alphabet[number % base];
-		number /= base;
-	}
-	
-	for (std::size_t i = 0; i < result.size() / 2; i++) {
-		std::swap(result[i], result[result.size() - i - 1]);
-	}
-	return result;
+std::string decimalToBase(int number, unsigned int base) {
+  if (base >= alphabet.length()) return "";
+
+  std::string result = "";
+
+  if (number < 0) {
+    result += '-';
+  }
+
+  while (number > 0) {
+    result += alphabet[number % base];
+    number /= base;
+  }
+
+  for (std::size_t i = 0; i < result.size() / 2; i++) {
+    std::swap(result[i], result[result.size() - i - 1]);
+  }
+  return result;
 }
 
-std::string to_hex(int number) {
-	return decimal_to_base(number, 16);
-}
+std::string toHex(int number) { return decimalToBase(number, 16); }
 
-std::string to_oct(int number) {
-	return decimal_to_base(number, 8);
-}
-
+std::string toOct(int number) { return decimalToBase(number, 8); }
 
 #include <iostream>
 
 int main() {
-	std::cout << "752 to base 8: " << to_oct(752) << std::endl;
-	std::cout << "752 to base 16: " << to_hex(752) << std::endl;
-	
-	return 0;
+  std::cout << "752 to base 8: " << toOct(752) << std::endl;
+  std::cout << "752 to base 16: " << toHex(752) << std::endl;
+
+  return 0;
 }

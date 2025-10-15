05bef7fdbc1fcd0dd86ef6a8af071426643361e2
Dmitriy Girak <rodnayasem@gmail.com>
2025-10-15 03:12:40 +0300

    Добавлена новая папка с тасками

---
+++ sem1/GirakDA/homework_1/task_1.cpp
@@ -0,0 +1,10 @@
+// The area of Square.
+#include <iostream>
+
+int main() {
+  double side;
+  std::cout << "Enter the side of the square: ";
+  std::cin >> side;
+  std::cout << "The area of your square is: " << side * side << std::endl;
+  return 0;
+}

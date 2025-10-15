d0bee8dbe304db4072092bd6ee2e60b10ef7af97
Anthonynaa <anthonynaa@yandex.ru>
2025-10-15 17:12:44 +0300

    Goggle c++ stylized

---
+++ sem1/NovikovAA/Mini HW1/task1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main() {
+  int a = 0;
+
+  while (true) {
+    std::cout << "Input the square's side length:" << std::endl;
+    std::cin >> a;
+    int S = a * a;
+    std::cout << "S = " << S << std::endl;
+  }
+}

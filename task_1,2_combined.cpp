a8f66fffca87a066549c0f917f320a8fc3e78a6f
Weltred <111679674+Weltred@users.noreply.github.com>
2025-10-15 18:59:08 +0300

    Add files via upload

---
+++ task_1,2_combined.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+int main()
+{
+	std::cout << "Square side:";
+	int var;
+	std::cin >> var;
+	var *= var;
+	std::cout << "Area of the square = ";
+	std::cout << var;
+	std::cout << "\nTrapezoid sides, then height:";
+	float a, b, h, polusum;
+	std::cin >> a, std::cin >> b, std::cin >> h;
+	polusum = (a + b) / 2;
+	std::cout << "Area of the trapezoid = ";
+	std::cout << polusum * h;
+
+	return 0;
+}

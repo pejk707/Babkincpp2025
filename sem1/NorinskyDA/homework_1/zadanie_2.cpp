074e8442d60bf8a16f370d002e30a5e0b4d51a74
Dmitry Norinsky <dimitriusn24@gmail.com>
2025-10-16 12:38:30 +0300

    mini hw1 and hw2 done

---
+++ sem1/NorinskyDA/homework_1/zadanie_2.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+  setlocale(0, "");
+
+  float up_side, down_side, height;
+  cout << "Для подсчёта площади трапеции введите следующие параметры: " << endl
+       << "Длина верхнего основания: " << endl;
+  cin >> up_side;
+  cout << "Длина нижнего основания: " << endl;
+  cin >> down_side;
+  cout << "Высота: " << endl;
+  cin >> height;
+  cout << "Площадь трапеции: " << (up_side + down_side) / 2 * height << endl;
+
+  return 0;
+}

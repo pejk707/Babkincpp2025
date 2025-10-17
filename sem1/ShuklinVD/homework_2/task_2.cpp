18bfa0e141ef16dd861c81097fefefd2d558d2d2
Vovok777 <vladimir.shuklin77@mail.ru>
2025-10-17 21:51:19 +0300

    hw2 done

---
+++ sem1/ShuklinVD/homework_2/task_2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <locale>
+bool checkHypotesis(int num) {
+  if (num < 1) {
+    return false;
+  }
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+      // std::cout << num << '\n'; - код для того, чтобы разработчик мог увидеть
+      // ход действий
+    } else {
+      num = (num * 3 + 1) / 2;
+      // std::cout << num << '\n'; - код для того, чтобы разработчик мог увидеть
+      // ход действий
+    }
+  }
+  return true;
+}
+int main() {
+  setlocale(LC_ALL, "");
+  int input;
+  std::cout << "введите натуральное число: ";
+  std::cin >> input;
+  if (checkHypotesis(input)) {
+    std::cout << "всё хорошо";
+  } else {
+    std::cout << "кто-то ввёл не натуральное число";
+  }
+
+  return 0;
+}

7c328aeef7a50d2b9f311de3fcc6f70139549a6c
Dmitry Norinsky <dimitriusn24@gmail.com>
2025-10-17 21:31:32 +0300

    Доработано: убрано using namespace std;

--- sem1/NorinskyDA/homework_1/zadanie_2.cpp
+++ sem1/NorinskyDA/homework_1/zadanie_2.cpp
@@ -1,19 +1,20 @@
 ﻿
 #include <iostream>
-using namespace std;
 
 int main() {
   setlocale(0, "");
 
   float up_side, down_side, height;
-  cout << "Для подсчёта площади трапеции введите следующие параметры: " << endl
-       << "Длина верхнего основания: " << endl;
-  cin >> up_side;
-  cout << "Длина нижнего основания: " << endl;
-  cin >> down_side;
-  cout << "Высота: " << endl;
-  cin >> height;
-  cout << "Площадь трапеции: " << (up_side + down_side) / 2 * height << endl;
+  std::cout << "Для подсчёта площади трапеции введите следующие параметры: "
+            << std::endl
+            << "Длина верхнего основания: " << std::endl;
+  std::cin >> up_side;
+  std::cout << "Длина нижнего основания: " << std::endl;
+  std::cin >> down_side;
+  std::cout << "Высота: " << std::endl;
+  std::cin >> height;
+  std::cout << "Площадь трапеции: " << (up_side + down_side) / 2 * height
+            << std::endl;
 
   return 0;
 }

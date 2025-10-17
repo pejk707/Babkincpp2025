a66eaa090f8c11d5486a22b0aa223278392b4b10
Диана Мельникова <diana.melnikova.alex2007@gmail.com>
2025-10-17 21:56:12 +0300

    mini hw done

---
+++ sem1/MelnikovaDA/homework_1/Task_1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+  float a;
+
+  while (true) {
+    std::cout << "Side of the square=";
+    std::cin >> a;
+
+    if (a <= 0) {
+      std::cout << "Error" << std::endl;
+    } else {
+      std::cout << "Square area=" << a * a << std::endl;
+    }
+  }
+}

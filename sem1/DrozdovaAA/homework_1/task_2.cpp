f75259ecb9e61a0dbcb4f1f025b6c834de630318
mAlina258 <drozdov.a.c2@gmail.com>
2025-10-17 19:16:10 +0300

    mini hw 1
    Задача 1 и задача 2 в дз 1

---
+++ sem1/DrozdovaAA/homework_1/task_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+// 2 задача
+int main() {
+  setlocale(LC_ALL, "Russian");
+  std::cout << "Введите первую сторону трапеции ";
+  float side_1, side_2, height, answer;
+  std::cin >> side_1;
+  std::cout << "Введите вторую сторону трапеции ";
+  std::cin >> side_2;
+  std::cout << "Введите высоту трапеции ";
+  std::cin >> height;
+  std::cout << "Ee площадь: ";
+  answer = ((side_1 + side_2) * height) / 2;
+  std::cout << answer;
+  return 0;
+}

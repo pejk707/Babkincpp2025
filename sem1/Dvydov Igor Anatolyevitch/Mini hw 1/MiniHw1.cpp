cbe09413e1e4a185cdfdce0e5f2a4f20e232f460
Mirror8rorriM <abcrty999@gmail.com>
2025-10-17 21:43:09 +0300

    rework mini hw 1 and 2

--- sem1/Dvydov Igor Anatolyevitch/Mini hw 1/MiniHw1.cpp
+++ sem1/Dvydov Igor Anatolyevitch/Mini hw 1/MiniHw1.cpp
@@ -1,41 +1,39 @@
-﻿
-#include <iostream>
-float SSquere(float x);
-float STrapezoid(float x, float y, float h);
-int main()
-{
-    while (true) {
-        int choise{0};
-        float x;
-        float y;
-        float h;
-        std::cout << "press 1 for squre area\npress 2 for trapezoid area"<< std::endl;
-        std::cin >> choise;
-        switch (choise) {
-        case 1:
-            std::cout << "input shape" << std::endl;
-            std::cin >> x;
-            std::cout << std::endl << "squre area = " << SSquere(x) << std::endl;
-            break;
-        case 2:
-            std::cout << "input 1st shape" << std::endl;
-            std::cin >> x;
-            std::cout << "input 2nd shape" << std::endl;
-            std::cin >> y;
-            std::cout << "input hight" << std::endl;
-            std::cin >> h;
-            std::cout << std::endl << "trapezoid area = " << STrapezoid(x,y,h) << std::endl;
-            break;
-        default:
-            std::cout << "wrong input" << std::endl;
-            break;
-        }
+﻿#include <iostream>
+float sSquere(float x);
+float sTrapezoid(float x, float y, float h);
+int main() {
+  int choise;
+  float x;
+  float y;
+  float h;
+  while (true) {
+    std::cout << "press 1 for squre area\npress 2 for trapezoid area" << std::endl;
+    std::cin >> choise;
+    switch (choise) {
+      case 1:
+        std::cout << "input shape" << std::endl;
+        std::cin >> x;
+        std::cout << std::endl << "squre area = " << sSquere(x) << std::endl;
+        break;
+      case 2:
+        std::cout << "input 1st shape" << std::endl;
+        std::cin >> x;
+        std::cout << "input 2nd shape" << std::endl;
+        std::cin >> y;
+        std::cout << "input hight" << std::endl;
+        std::cin >> h;
+        std::cout << std::endl << "trapezoid area = " << sTrapezoid(x,y,h) << std::endl;
+        break;
+      default:
+        std::cout << "wrong input" << std::endl;
+        break;
     }
-    return 0;
+  }
+  return 0;
 }
-float SSquere(float x) {
-    return x * x;
+float sSquere(float x) {
+  return x * x;
 }
-float STrapezoid(float x, float y, float h) {
-    return (x + y) / 2 * h;
+float sTrapezoid(float x, float y, float h) {
+  return (x + y) / 2 * h;
 }
--- sem1/Dvydov Igor Anatolyevitch/Mini hw 2/MiniHomeWork2Task1.cpp
+++ sem1/Dvydov Igor Anatolyevitch/Mini hw 2/MiniHomeWork2Task1.cpp
@@ -2,28 +2,27 @@
 
 #include <iostream>
 void to16And8System(const int x);
-int main()
-{
-    int x;
-    std::cout << "input int: ";
-    std::cin >> x;
-    std::cout << std::endl;
-    to16And8System(x);
+int main() {
+  int x;
+  std::cout << "input int: ";
+  std::cin >> x;
+  std::cout << std::endl;
+  to16And8System(x);
 }
 void to16And8System(const int x) {
-    std::string result = "";
-    const std::string symbols = "0123456789abcdef";
-    int vremX = x;
-    while (vremX) {
-        result = symbols[vremX % 16] + result;
-        vremX /= 16;
-    }
-    std::cout << "in 16 system " << result << std::endl;
-    vremX = x;
-    result = "";
-    while (vremX) {
-        result = symbols[vremX % 8] + result;
-        vremX /= 8;
-    }
-    std::cout << "in 8 system " << result << std::endl;
+  std::string result = "";
+  const std::string symbols = "0123456789abcdef";
+  int vremX = x;
+  while (vremX) {
+    result = symbols[vremX % 16] + result;
+    vremX /= 16;
+  }
+  std::cout << "in 16 system " << result << std::endl;
+  vremX = x;
+  result = "";
+  while (vremX) {
+    result = symbols[vremX % 8] + result;
+    vremX /= 8;
+  }
+  std::cout << "in 8 system " << result << std::endl;
 }

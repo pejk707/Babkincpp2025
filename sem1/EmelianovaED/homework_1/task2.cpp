9b742805c362d099666993121cc4408a54577edc
Vilorik2507 <138361479+Vilorik2507@users.noreply.github.com>
2025-10-17 20:17:03 +0300

    mini HM_1
    Edited homework1

--- sem1/EmelianovaED/homework_1/task2.cpp
+++ sem1/EmelianovaED/homework_1/task2.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-int trapezeS(int side_a, int side_b, int hight) {
-  int square_tra = ((side_a + side_b) * hight) / 2;
-  return square_tra;
+int trapezoidArea(int side_a, int side_b, int hight) {
+  int area_tra = ((side_a + side_b) * hight) / 2;
+  return area_tra;
 }
 int main() {
   int side_a = 24, side_b = 13, hight = 15;
-  std::cout << trapezeS(side_a, side_b, hight);
+  std::cout << trapezoidArea(side_a, side_b, hight);
 
   return 0;
 }

d48aaef777774a78bb693d158e1902b7492ccc72
cherednichenkobrand <provizorvanya@mail.ru>
2025-10-24 20:21:40 +0300

    Дз1 Версия 2
    Сделано форматирование Google, убрал using namecpace std

---
+++ sem1/CherednichenkoIA/homework_1/Task1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int S(int a, int b) {
+  std::cout << "S = " << a * b;
+  return 0;
+}
+
+int main() {
+  int a, b;
+  std::cout << "a = ";
+  std::cin >> a;
+
+  std::cout << "b = ";
+  std::cin >> b;
+  S(a, b);
+}
